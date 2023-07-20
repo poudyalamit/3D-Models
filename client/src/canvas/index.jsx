f29757d3378e104abde9e1edd49928f62a3353a1
poudyalamit <poudyalamit20@gmail.com>
2023-07-20 19:01:24 +0545

    dded shirt with stylings

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,21 @@
-const Canvas = () => {
+import { Canvas } from "@react-three/fiber"
+import {Environment, Center, Backdrop} from "@react-three/drei"
+import CameraRig from "./CameraRig"
+import Shirt from "./Shirt"
+
+const CanvasModel = () => {
   return (
-    <div>
-      
-    </div>
+  <Canvas>
+    <ambientLight intensity={0.5} />
+    <Environment preset="city"/>
+    <CameraRig>
+    {/* <Backdrop/> */}
+    <Center>
+      <Shirt/>
+    </Center>
+    </CameraRig>
+  </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
